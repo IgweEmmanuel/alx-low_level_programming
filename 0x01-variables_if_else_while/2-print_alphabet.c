c9e11a7a0880af667cbf6fe43cfb1a5aa7b40317
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-06 03:45:22 -0700

    Print alphabet to lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = tolower('M');
+
+	putchar(c);
+	return (0);
+}
