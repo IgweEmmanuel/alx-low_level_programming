56c4883c023393f1466c144c98c8df3ffd2b6404
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-06 09:04:46 -0700

    Combine

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, b;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (b = a + 1; b <= 9; b++)
+		{
+				
+			putchar((i % 10) + '0');
+			putchar((b % 10) + '0');
+			if (i == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
