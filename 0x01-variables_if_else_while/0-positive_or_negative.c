fdefd03de085958be8b964ce6b3a81dd599184b4
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-06 02:36:11 -0700

    Check random

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -14,11 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0)
+	if( n > 0 )
 		printf("%d", "is positive\n");
-	else if(n==0)
+	else if( n == 0 )
 		printf("%d", "is zero\n");
-	else if(n<0)
+	else if( n < 0 )
 		printf("%d", "is negative\n");
 	return (0);
 }
