b9fc750bbb98247ad97540313c776abaeb502da1
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-06 09:20:46 -0700

    Tripple combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, b;
+
+	for (i = 0; i <= 8; i++)
+	{
+		for (b = i + 1; b <= 9; b++)
+		{
+			putchar((i % 10) + '0');
+			putchar((b % 10) + '0');
+			if (i == 8 && b == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
