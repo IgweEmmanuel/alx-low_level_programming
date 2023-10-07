9161c8aee079a49d03377adee18aa75435a9222f
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-07 11:47:30 -0700

    Two two-units

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,6 +16,7 @@ int main(void)
 			putchar((i % 10) + '0');
 			putchar((b % 10) + '0');
 			putchar(' ');
+			++b;
 			putchar((i % 10) + '0');
 			putchar((b % 10) + '0');
 			if (i == 8 && b == 9)
