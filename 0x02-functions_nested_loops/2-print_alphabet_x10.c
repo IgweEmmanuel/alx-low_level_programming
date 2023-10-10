cbd1bc85f883360c16c3c5f1d1d2b176dd0e848e
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-10 16:35:51 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Emmanuel Igwe
+ */
+
+#include "main.h"
+
+/**
+ * 2-print_alphabet_x10  - Prints the alphabet 10 times.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int b;
+
+	for (b = 0; b <= 10; b++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
