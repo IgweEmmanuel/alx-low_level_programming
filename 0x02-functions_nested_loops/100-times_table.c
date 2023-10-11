9646c7f743bfdc1cd1bd575df5285926c88af611
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-11 10:58:12 -0700

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,14 +24,11 @@ void print_times_table(int n)
 					_putchar(c);
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
 				}
 				if (b <= 99)
 				{
 					_putchar(c);
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 				if (b >= 100)
