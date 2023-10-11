68aa936f40bcf9f2115a086f44302270bd3ddb1a
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-10 22:00:02 -0700

    Abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,9 @@
 /*
  * File: 6-abs.c
- * Auth: Brennan D Baraban
+ * Auth: Emmanuel Igwe
  */
 
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _abs - Computes the absolute value of an integer.
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet_x10(void);
 int _islower(int i);
 int _isalpha(int x);
 int print_sign(int n);
-int abs(int n);
+int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
