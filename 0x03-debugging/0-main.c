51268adc952ead4c36ad2f3f2c7c998067e7aa79
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-12 21:41:58 -0700

    Positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,16 @@
-/*
- * File: 0-main that returns the value of i
- * Auth: Emmanuel
- */
 #include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
-*
 * Return: 0
 */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
