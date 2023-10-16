de1ef3236d1027f2bb4c5055a15ccd730d89dcf3
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-16 11:38:05 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+/*
+ * File: 0x05-pointers_arrays_strings
+ * Auth: Emmanuel
+ */
+
+#include "main.h"
+/**
+ * reset_to_98(int *n) - This is the reset function
+ * @n: is used to change the value of n
+ *
+ * Return: the return value is void
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
