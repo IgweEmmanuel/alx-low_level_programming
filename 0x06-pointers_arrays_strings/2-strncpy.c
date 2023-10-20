2ae7623251713cfd86d75a9e06f0c00876a13240
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-19 23:17:53 -0700

    Copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strncat - This concatenates the strings
+ * _strncpy - This concatenates the strings
  * @dest: This is the first string
  * @src: This is the second string
  * @n: variable integer
  * Return: Always returns value char
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 
