36844cf3916f7e1ec27d88ec7b7b8448ae3c0023
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-25 15:17:44 -0700

    Puts recursion

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - This checks hello world.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("Hellow World");
+	return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Readme dile for recursion
