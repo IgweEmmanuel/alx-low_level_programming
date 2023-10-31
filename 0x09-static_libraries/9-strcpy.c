b4c89cc52fc92cc71359a16312e83a5471052af3
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2023-10-31 17:13:23 +0000

    Library

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcpy - this is used to copy strings
+ * @dest: is the varialbe
+ * @src: is the second variable
+ * 
+ * Returns: always returns a value
+ */ 
+char *_strcpy(char *dest, char *src){
+
+}
