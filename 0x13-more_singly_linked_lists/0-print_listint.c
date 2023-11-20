a9332e95c72f513d289d42b2f2b7c18ce0193ce1
chigozieigweemmanuel@gmail.com <chigozieigweemmanuel@gmail.com>
2023-11-20 18:20:23 +0000

    Count nodes in a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,20 @@
 
 /**
  * print_listint - Prints lists node
+ * @h: a head pointing to the next node
  *
  * Return: returns size_t
  */
 
 size_t print_listint(const listint_t *h)
 {
-    size_t count;
+	size_t count;
 
-    for (count = 0; h != NULL; count++)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-    }
+	for (count = 0; h != NULL; count++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 
-    return (count);
+	return (count);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,11 +2,18 @@
 #define LISTS_H
 #include <stddef.h>
 #include <stdio.h>
+
+/**
+ * struct node - sturctured data for linked lists
+ * @n: this is n for first variable member
+ * @next: points to the next node
+ */
+
 typedef struct node
 {
 	int n;
 	struct node *next;
-}listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 
