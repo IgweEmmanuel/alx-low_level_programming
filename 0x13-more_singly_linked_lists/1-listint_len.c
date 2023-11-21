dee5e34d210350a77010d6171d276d0ad7a13620
chigozieigweemmanuel@gmail.com <chigozieigweemmanuel@gmail.com>
2023-11-21 11:43:48 +0000

    Counts the elements in a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,18 @@
 #include "lists.h"
 
 /**
- * listint_t - This function will count the number of elements in a linked list
+ * listint_len - Counts the number of elements in a linked list
  * @h: This is the head parameter that points to the first node
  *
  * Return: This returns unsigned int
  */
-
 size_t listint_len(const listint_t *h)
 {
 	size_t count;
+
 	for (count = 0; h->next != NULL; count++)
 	{
 		h = h->next;
-	}	
-	return(++count);
+	}
+	return (++count);
 }
