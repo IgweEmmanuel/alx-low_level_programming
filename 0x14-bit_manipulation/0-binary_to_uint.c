f1d30be1cb9c1036fcbc759e6f326357aa9a57bd
chigozieigweemmanuel@gmail.com <chigozieigweemmanuel@gmail.com>
2023-11-24 05:01:29 +0000

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stddef.h>
 /**
  * binary_to_uint - convert binary string to decimal
  * @b: binary string
@@ -9,21 +9,21 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal;
+	unsigned int ans;
 	int i;
 
-	decimal = 0;
+	ans = 0;
+	if (b == NULL)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (!b)
-			return (0);
 		if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
+			ans = (ans << 1) | 1;
 		else if (b[i] == '0')
-			decimal <<= 1;
+			ans <<= 1;
 		else if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
 
-	return (decimal);
+	return (ans);
 }
