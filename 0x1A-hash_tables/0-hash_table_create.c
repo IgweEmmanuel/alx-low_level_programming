32ba0e69225d8e7a502ffca77c521797b86efcaa
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-02 10:51:46 +0000

    this commit is about creating new hash table by dynamically assigning sizes to the table and its associative arrays

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * *hash_table_create - This function creates a hash table
+ * @size: This is the size parameter for the key
+ *
+ * Return: This returns a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+
+	if (new_table == NULL)
+	{
+		free(new_table);
+		return NULL;
+	}
+	new_table->size = size;
+
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+	{
+		free(new_table);
+		return NULL;
+	}
+
+	for (i = 0; i < size; i++)
+		new_table->array = NULL;
+
+	return new_table;
+}
