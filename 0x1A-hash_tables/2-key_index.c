8a3578d5b195b858bff5c621615932fdd04340ee
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-02 12:40:05 +0000

    This uses a class to call a function with object of the class as a parameter

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int hash_code;
 	unsigned long int index;
 
-	hash_code = hash_djb2(key);	
+	hash_code = hash_djb2(key);
 
 	index  = hash_code % size;
 	return (index);
