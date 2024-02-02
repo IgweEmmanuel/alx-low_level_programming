592a3c6b4e0b5b0e43edd5c65558c1f5d3b024be
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-02-02 10:56:28 +0000

    This uses a class to call a function with object of the class as a parameter

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,10 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
-	{
-		free(new_table);
-		return NULL;
-	}
+		return (NULL);
+
 	new_table->size = size;
 
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
@@ -24,11 +22,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table->array == NULL)
 	{
 		free(new_table);
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
 		new_table->array = NULL;
 
-	return new_table;
+	return (new_table);
 }
