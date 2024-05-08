86066183357e6e84a93cc2bc3a714e4c56f24863
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-05-08 16:04:20 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,14 +14,13 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+	if (array == NULL)
+		return (-1);
 
+	for (i = 0; i < size; i++){
 		if (array[i] == value)
-		{
-			printf("Found %d at index: %ld\n\n", value, i);
-		}	
+			return (int)i;
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
 	return (1);
 }
